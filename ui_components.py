57ba7d1fd08f3989250ae50b02b9e7bda75639f0
Jeff Oberlander <jeffoberlander@gmail.com>
2024-06-03 14:39:32 -0700

    remove cacheing on plotly function until streamlit resolves

--- ui_components.py
+++ ui_components.py
@@ -425,7 +425,7 @@ def levels_line_chart(daterange, countries_list, app="Both", language="All"):
     st.plotly_chart(fig, use_container_width=True)
 
 
-@st.cache_data(ttl="1d", show_spinner=False)
+# @st.cache_data(ttl="1d", show_spinner=False)
 def funnel_change_line_chart(
     daterange=default_daterange, language=["All"], countries_list=["All"], toggle=""
 ):
@@ -502,97 +502,127 @@ def funnel_change_line_chart(
 
         df = pd.concat([df.reset_index(drop=True), entry], ignore_index=True)
 
+    df["start_date"] = df["start_date"]
     try:
-        df["start_date"] = df["start_date"]
         df["DC over LR"] = np.where(df["LR"] == 0, 0, (df["DC"] / df["LR"]) * 100)
         df["DC over LR"] = df["DC over LR"].astype(int)
+    except ZeroDivisionError:
+        df["DC over LR"] = 0
 
+    try:
         df["TS over LR"] = np.where(df["LR"] == 0, 0, (df["TS"] / df["LR"]) * 100)
         df["TS over LR"] = df["TS over LR"].astype(int)
+    except ZeroDivisionError:
+        df["TS over LR"] = 0
 
+    try:
         df["TS over DC"] = np.where(df["DC"] == 0, 0, (df["TS"] / df["DC"]) * 100)
         df["TS over DC"] = df["TS over DC"].astype(int)
+    except ZeroDivisionError:
+        df["TS over DC"] = 0
 
+    try:
         df["SL over LR"] = np.where(df["LR"] == 0, 0, (df["SL"] / df["LR"]) * 100)
         df["SL over LR"] = df["SL over LR"].astype(int)
+    except ZeroDivisionError:
+        df["SL over LR"] = 0
 
+    try:
         df["SL over TS"] = np.where(df["TS"] == 0, 0, (df["SL"] / df["TS"]) * 100)
         df["SL over TS"] = df["SL over TS"].astype(int)
+    except ZeroDivisionError:
+        df["SL over TS"] = 0
 
+    try:
         df["PC over LR"] = np.where(df["LR"] == 0, 0, (df["PC"] / df["LR"]) * 100)
         df["PC over LR"] = df["PC over LR"].astype(int)
+    except ZeroDivisionError:
+        df["PC over LR"] = 0
 
+    try:
         df["PC over SL"] = np.where(df["SL"] == 0, 0, (df["PC"] / df["SL"]) * 100)
         df["PC over SL"] = df["PC over SL"].astype(int)
+    except ZeroDivisionError:
+        df["PC over SL"] = 0
 
+    try:
         df["LA over LR"] = np.where(df["LR"] == 0, 0, (df["LA"] / df["LR"]) * 100)
         df["LA over LR"] = df["LA over LR"].astype(int)
+    except ZeroDivisionError:
+        df["LA over LR"] = 0
 
+    try:
         df["LA over PC"] = np.where(df["PC"] == 0, 0, (df["LA"] / df["PC"]) * 100)
         df["LA over PC"] = df["LA over PC"].astype(int)
+    except ZeroDivisionError:
+        df["LA over PC"] = 0
 
+    try:
         df["GC over LR"] = np.where(df["LR"] == 0, 0, (df["GC"] / df["LR"]) * 100)
         df["GC over LR"] = df["GC over LR"].astype(int)
+    except ZeroDivisionError:
+        df["GC over LR"] = 0
 
+    try:
         df["GC over LA"] = np.where(df["LA"] == 0, 0, (df["GC"] / df["LA"]) * 100)
         df["GC over LA"] = df["GC over LA"].astype(int)
+    except ZeroDivisionError:
+        df["GC over LA"] = 0
 
-        if toggle == "Compare to Previous":
+    if toggle == "Compare to Previous":
 
-            df2 = df[
-                [
-                    "start_date",
-                    "DC over LR",
-                    "TS over DC",
-                    "SL over TS",
-                    "PC over SL",
-                    "LA over PC",
-                    "GC over LA",
-                ]
+        df2 = df[
+            [
+                "start_date",
+                "DC over LR",
+                "TS over DC",
+                "SL over TS",
+                "PC over SL",
+                "LA over PC",
+                "GC over LA",
             ]
-        else:
-            df2 = df[
-                [
-                    "start_date",
-                    "DC over LR",
-                    "TS over LR",
-                    "SL over LR",
-                    "PC over LR",
-                    "LA over LR",
-                    "GC over LR",
-                ]
+        ]
+    else:
+        df2 = df[
+            [
+                "start_date",
+                "DC over LR",
+                "TS over LR",
+                "SL over LR",
+                "PC over LR",
+                "LA over LR",
+                "GC over LR",
             ]
+        ]
 
-        df2["start_date"] = pd.to_datetime(df2["start_date"])
+    df2["start_date"] = pd.to_datetime(df2["start_date"])
 
-        # Create traces for each column
-        traces = []
-        for column in df2.columns[1:]:
-            traces.append(
-                go.Scatter(
-                    x=df2["start_date"],
-                    y=df2[column],
-                    mode="lines+markers",
-                    name=column,
-                    hovertemplate="%{y}%<br>",
-                )
+    # Create traces for each column
+    traces = []
+    for column in df2.columns[1:]:
+        traces.append(
+            go.Scatter(
+                x=df2["start_date"],
+                y=df2[column],
+                mode="lines+markers",
+                name=column,
+                hovertemplate="%{y}%<br>",
             )
-
-        # Create layout
-        layout = go.Layout(
-            title="Line Chart",
-            xaxis=dict(title="Date"),
-            yaxis=dict(title="Percent"),
         )
 
-        # Create figure
-        fig = go.Figure(data=traces, layout=layout)
-        st.plotly_chart(fig, use_container_width=True)
+    # Create layout
+    layout = go.Layout(
+        title="Line Chart",
+        xaxis=dict(title="Date"),
+        yaxis=dict(title="Percent"),
+    )
 
-        st.dataframe(df, hide_index=True)
+    # Create figure
+    fig = go.Figure(data=traces, layout=layout)
+    st.plotly_chart(fig, use_container_width=True)
 
-    except Exception as e:
-        st.write("No data")
+
+#   st.dataframe(df, hide_index=True)
 
 
 def top_campaigns_by_downloads_barchart(n):
@@ -618,7 +648,7 @@ def top_campaigns_by_downloads_barchart(n):
     )
 
 
-@st.cache_data(ttl="1d", show_spinner=False)
+# @st.cache_data(ttl="1d", show_spinner=False)
 def funnel_change_by_language_chart(
     languages, countries_list, daterange, upper_level, bottom_level
 ):
