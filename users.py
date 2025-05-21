242ec74a0279ee802eec0a5485b6f1914da006df
Jeff Oberlander <joberlander@curiouslearning.org>
2025-05-21 09:54:35 -0700

    move back to non incremental table for app launch

--- users.py
+++ users.py
@@ -42,7 +42,7 @@ async def get_users_list():
 
         sql_cr_app_launch = f"""
             SELECT *
-            FROM `dataexploration-193817.user_data.cr_app_launch_inc`
+            FROM `dataexploration-193817.user_data.cr_app_launch`
             WHERE first_open BETWEEN PARSE_DATE('%Y/%m/%d','{start_date}') AND CURRENT_DATE()
         """
 
