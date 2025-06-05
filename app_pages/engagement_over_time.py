fd5b242856b3e6122dd555969b2c00a29530c4f9
Jeff Oberlander <joberlander@curiouslearning.org>
2025-06-05 09:12:23 -0700

    add new engagement blank page

---
+++ app_pages/engagement_over_time.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from rich import print as rprint
+from millify import prettify
+import ui_components as uic
+import ui_widgets as ui
+import users
+import settings
+import metrics
+
+
+settings.initialize()
+settings.init_user_list()
+
+ui.display_definitions_table("Definitions",ui.level_percent_definitions)
+
+col1, col2 = st.columns(2)
+
+languages = users.get_language_list()
+countries_list = users.get_country_list()
+
+with col1:
+    language = ui.single_selector(
+        languages, placement="col", title="Select a language", key="crf-2"
+    )   
+with col2:
+    countries_list = ui.multi_select_all(
+        countries_list,
+        title="Country Selection",
+        key="LA_LR_Time",
+        placement="middle",
+    )
+
+    selected_date, option = ui.calendar_selector(placement="middle", key="DC-1", index=0, title="Select user cohort by date")
+    daterange = ui.convert_date_to_range(selected_date, option)
+
+
+if len(countries_list) > 0  and (len(daterange) == 2):
+    user_cohort_list = metrics.get_user_cohort_list(daterange=daterange,languages=language,countries_list=countries_list,app="CR")
+    
