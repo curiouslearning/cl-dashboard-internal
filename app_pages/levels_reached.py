97fb870c2b932b3dbad20082259b4fd3b6bb15c0
Jeff Oberlander <joberlander@curiouslearning.org>
2025-08-15 10:57:16 -0700

    added unity to levels_reached

---
+++ app_pages/levels_reached.py
@@ -0,0 +1,13 @@
+
+
+from ui_components import levels_reached_chart
+
+
+from settings import initialize
+initialize()
+from users import ensure_user_data_initialized
+ensure_user_data_initialized()
+
+levels_reached_chart()
+        
+    
